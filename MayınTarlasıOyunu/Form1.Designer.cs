666f7249808bca5b515f9a41d6706ee32a18462c
Asus <Asus@LAPTOP-I94IGVVP>
2022-06-12 12:06:36 +0300

    Add project files.

---
+++ MayınTarlasıOyunu/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace MayınTarlasıOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnUret = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMayın = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(626, 265);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnUret
+            // 
+            this.btnUret.Location = new System.Drawing.Point(656, 12);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(132, 140);
+            this.btnUret.TabIndex = 1;
+            this.btnUret.Text = "Üret";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(656, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SKOR";
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Location = new System.Drawing.Point(709, 184);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(0, 17);
+            this.lblSkor.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(659, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MAYIN";
+            // 
+            // lblMayın
+            // 
+            this.lblMayın.AutoSize = true;
+            this.lblMayın.Location = new System.Drawing.Point(729, 222);
+            this.lblMayın.Name = "lblMayın";
+            this.lblMayın.Size = new System.Drawing.Size(0, 17);
+            this.lblMayın.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 298);
+            this.Controls.Add(this.lblMayın);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUret);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnUret;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMayın;
+    }
+}
+
---
+++ MayınTarlasıOyunu/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayınTarlasıOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblSkor.Text = "0";
+            lblMayın.Text = "0";
+        }
+
+        private void btnUret_Click(object sender, EventArgs e)
+        {
+            int mayin1 = 0;
+            int mayin2 = 0;
+            int mayin3 = 0;
+
+            Random rnd = new Random();
+            mayin1 = rnd.Next(1, 20);
+            mayin2 = rnd.Next(21, 35);
+            mayin3 = rnd.Next(35, 50);
+
+            for (int i = 1; i <= 50; i++)
+            {
+                Button btnTemp = new Button();
+                btnTemp.Name = "btn" + i.ToString();
+                btnTemp.Size = new System.Drawing.Size(35, 35);
+                btnTemp.Text = i.ToString();
+                btnTemp.UseVisualStyleBackColor = true;
+                if (mayin1 == i || mayin2 == i || mayin3 == i)
+                {
+                    btnTemp.Tag = true;
+                }
+                else
+                {
+                    btnTemp.Tag = false;
+                }
+
+                btnTemp.Click += btnTemp_Click;
+                flowLayoutPanel1.Controls.Add(btnTemp);
+  
+            }
+        }
+        private void btnTemp_Click(object sender, EventArgs e)
+        {
+
+            Button basilanButon = ((Button)sender);
+            bool mayinBulundumu = (bool)basilanButon.Tag;
+
+            if (mayinBulundumu)
+            {
+                MessageBox.Show("Mayını buldunuz, tebrikler");
+                basilanButon.BackColor = Color.Red;
+               // basilanButon.Enabled = false;
+                int mayınSkor = int.Parse(lblMayın.Text);
+                mayınSkor++;
+                lblMayın.Text = mayınSkor.ToString();
+            }
+            else
+            {
+                basilanButon.BackColor = Color.Green;
+                //basilanButon.Enabled = false;
+                int skorInt = int.Parse(lblSkor.Text);
+                skorInt++;
+                lblSkor.Text = skorInt.ToString();
+            }
+
+            if (lblMayın.Text == "3")
+            {
+                MessageBox.Show("Uygulama bitti...");
+
+                for (int i = 1; i <= 50; i++)
+                {
+                    flowLayoutPanel1.Controls["btn" + i.ToString()].Enabled = false;
+                }
+            }
+
+        }
+
+    }
+}
+
